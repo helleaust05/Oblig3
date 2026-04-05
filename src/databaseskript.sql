65999d6769eed35985071a3f332da288571d01b4
helleaust05 <helleaustgulen05@gmail.com>
2026-04-06 00:07:31 +0200

    wiii2

---
+++ src/databaseskript.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Ansatt (
+    ansattId Int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    brukernavn varchar(4) NOT NULL UNIQUE,
+    fornavn varchar(30) NOT NULL,
+    etternavn varchar(50) NOT NULL,
+    ansattDato date,
+    stilling varchar(50),
+    manedslonn decimal(10, 2)
+);
+
+INSERT INTO ansatt (brukernavn, fornavn, etternavn, ansattDato, stilling, manedslonn)
+VALUES
+    ('lihe', 'Lilly', 'Henriksen', '2017-06-24', 'Nugattispiser', 33333,3),
+    ('tola', 'Tonny','Larsen', '2011-09-05', 'professor', 98394);
--- src/main/java/classes/Ansatt.java
+++ src/main/java/classes/Ansatt.java
@@ -14,27 +14,52 @@ public class Ansatt {
     private String brukernavn;
 
     @Column
-    private String Fornavn;
+    private String fornavn;
 
     @Column
-    private String Etternavn;
+    private String etternavn;
 
     @Column(name = "TidAnsettelse")
-    private java.time.LocalDate AnsettelseDato;
+    private java.time.LocalDate ansettelseDato;
 
     @Column
     private String stilling;
 
     @Column
-    private double Månedslonn;
+    private double manedslonn;
 
   //Tom konstruktør
     public Ansatt() {}
 
   //Konstruktør
-    public Ansatt() {
-
+    public Ansatt(String brukernavn, String fornavn, String etternavn, String stilling, double manedslonn) {
+        this.brukernavn = brukernavn;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.stilling = stilling;
+        this.manedslonn = manedslonn;
+        this.ansettelseDato = java.time.LocalDate.now();
     }
 
+    public Integer getId() {return id;}
 
+    public String getBrukernavn() {return brukernavn;}
+    public void setBrukernavn(String brukernavn) {this.brukernavn = brukernavn;}
+
+    public String getFornavn() {return fornavn;}
+    public void setFornavn(String fornavn) {this.fornavn = fornavn;}
+
+
+    public String getEtternavn() {return etternavn;}
+    public void setEtternavn(String etternavn) {this.etternavn = etternavn;}
+
+    public String getStilling() {return stilling;}
+    public void setStilling() {this.stilling = stilling;}
+
+    public double getManedslonn() {return manedslonn;}
+    public void setManedslonn(Double manedslonn) {this.manedslonn = manedslonn;}
+
+    public java.time.LocalDate getAnsettelseDato() {return ansettelseDato;}
+    public void setAnsettelseDato(java.time.LocalDate) {this.ansettelseDato = ansettelseDato;}
 }
+
