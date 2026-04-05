13642d111b6e844089c374903a7034f7a450702a
helleaust05 <helleaustgulen05@gmail.com>
2026-04-05 23:13:15 +0200

    wiii

--- src/main/java/classes/Ansatt.java
+++ src/main/java/classes/Ansatt.java
@@ -9,11 +9,29 @@ public class Ansatt {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String brukernavn; //Unikt**?
-    private String navn;
 
+    @Column(unique = true)
+    private String brukernavn;
+
+    @Column
+    private String Fornavn;
+
+    @Column
+    private String Etternavn;
+
+    @Column(name = "TidAnsettelse")
+    private java.time.LocalDate AnsettelseDato;
+
+    @Column
+    private String stilling;
+
+    @Column
+    private double Månedslonn;
+
+  //Tom konstruktør
     public Ansatt() {}
 
+  //Konstruktør
     public Ansatt() {
 
     }
